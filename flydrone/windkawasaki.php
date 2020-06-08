53ed3613fd7e766aa6144d38f9dbd9ed98c7b0d9
Linuxkay <sccck000@gmail.com>
2020-06-09 07:47:24 +0900

    xpath fixed and updated

--- flydrone/windkawasaki.php
+++ flydrone/windkawasaki.php
@@ -7,7 +7,8 @@ $cli = new Goutte\Client();
 //crawl wind info 4 Kawasaki 
 $urlkawasaki = 'http://weathernews.jp/onebox/38.18/140.64/';
 $crawlerkawasaki = $cli->request('GET', $urlkawasaki);
-$kawasaki = $crawlerkawasaki->filterXPath('//*[@id="content_main"]/div[3]/div[1]/div[1]/div/table[2]/tr[3]/td[2]')->text();
+$kawasaki = $crawlerkawasaki->filterXPath('//*[@id="main"]/section[6]/div/div/div[2]/ul/li[5]')->text();
+#$kawasaki = $crawlerkawasaki->filterXPath('//*[@id="content_main"]/div[3]/div[1]/div[1]/div/table[2]/tr[3]/td[2]')->text();
 //$kawasaki = preg_replace('[\xFF01-\xFF5E]','', $kawasaki);
 //$kawasaki = mb_ereg_replace("¡§ËÌ","", $kawasaki);
 $kawasaki = mb_substr($kawasaki, -5, 5);
